d0040ab6d1f4fff8cd3bf27606efa846b7d79d95
mithereal <mithereal@gmail.com>
2023-04-02 03:23:26 -0700

    .

--- test/ex_inventory_test.exs
+++ test/ex_inventory_test.exs
@@ -1,8 +1,3 @@
 defmodule ExInventoryTest do
-  use ExUnit.Case
-  doctest ExInventory
-
-  test "greets the world" do
-    assert ExInventory.hello() == :world
-  end
+  use Warehouse.DataCase
 end
