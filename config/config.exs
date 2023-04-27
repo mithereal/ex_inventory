ea3148d99994a4d09a6fd05d11fcbd53c82aab8a
mithereal <mithereal@gmail.com>
2023-04-26 17:47:30 -0700

    .

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,6 @@
 # and its dependencies with the aid of the Mix.Config module.
 import Config
 
-config :ex_inventory, :ecto_repos, [ExInventory.Repo.Null]
+config :ex_inventory, :ecto_repos, [ExInventory.Repo]
 
 import_config "#{Mix.env()}.exs"
