a3483cc9813a4975ab745c73646d0ab130426755
mithereal <mithereal@gmail.com>
2023-04-26 17:47:48 -0700

    .

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,6 @@ import Config
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
-# config :ex_inventory, :ecto_repos, [ExInventory.Repo.Null]
-
 config :ex_inventory, :ecto_repos, [ExInventory.Repo]
 
 config :ex_inventory, ExInventory.Repo,
